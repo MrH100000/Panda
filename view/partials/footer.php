176e50ff335a0455e94482d4d797ce1883ac52f9
turne292 <95255291+turne292@users.noreply.github.com>
2022-04-19 16:05:01 -0500

    Update footer.php

--- view/partials/footer.php
+++ view/partials/footer.php
@@ -1,2 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>ITS 362 - Panda Shop </title>
+    <link rel="stylesheet" type="text/css" href="ITS362-lab08-DanielleTurner.css">
+</head>
+<body>
+    <footer> 
+        <p>ITS 362 - Panda Shop </p>
+        <p>David Higley - Danielle Turner </p>
+        <p> <?php echo date("Y"); ?></p>
+    </footer>
 </body>
 </html>
