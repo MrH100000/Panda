d372dd6c0cb1d746731d12d135523bb4246d507b
turne292 <95255291+turne292@users.noreply.github.com>
2022-04-19 16:09:53 -0500

    Update header.php

--- view/partials/header.php
+++ view/partials/header.php
@@ -4,7 +4,7 @@
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="panda.css">
-    <title>Store</title>
+    <title>Panda Shop</title>
 </head>
 
 <body>
