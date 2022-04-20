ee65f08c877f37b2c8fe9b0da660bab6525a56b3
David Higley <5118164+MrH100000@users.noreply.github.com>
2022-04-20 12:14:15 -0500

    Fix footer

--- view/partials/footer.php
+++ view/partials/footer.php
@@ -1,14 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="panda.css">
-</head>
-<body>
-    <footer> 
+    <div class="footer"> 
         <p>ITS 362 - Panda Shop </p>
         <p>David Higley - Danielle Turner </p>
         <p> <?php echo date("Y"); ?></p>
-    </footer>
+    </div>
 </body>
 </html>
