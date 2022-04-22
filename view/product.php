4b548757b5a5f25076b670132dc15f503413bcd9
Danielle <turne292@pnw.edu>
2022-04-22 13:20:11 -0500

    product card more

--- view/product.php
+++ view/product.php
@@ -1,10 +1,13 @@
 <?php require_once __DIR__ . '/partials/header.php'; ?>
 <center>
-<h2>All Products:</h2>
+<div>
+<h1>All Products:</h1>
+</div>
     <?php foreach ($productList as $product): ?>
 	    <div class='productCard'>
+            <br>
             <img src="<?php echo $product['ProductImage']?>" alt="<?php echo $product["Name"]; ?>" style="height:150px">
-            <h1><?php echo $product["Name"]; ?> </h1>
+            <h2><?php echo $product["Name"]; ?> </h2>
             <p><?php echo "$". number_format($product["Price"]); ?></p>
             <p><?php echo $product["Description"]; ?> </p>
             <p> <button class='productButton'> Add to Cart </button></p>
