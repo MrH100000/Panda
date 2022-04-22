4652719db548da32eed8095ea2db807e0c4e88cb
Danielle <turne292@pnw.edu>
2022-04-22 14:04:15 -0500

    about us page

---
+++ view/aboutUs.php
@@ -0,0 +1,8 @@
+<?php require_once __DIR__ . '/partials/header.php'; ?>
+<center>
+    <h1>Welcome to MVC Panda!</h1>
+</center>
+<p>Welcome to our page, this was created by David Higley and Danielle Turner</p>
+<p>We have gone down the Panda Diplomacy rabbit hole so here we are. This is what we are doing now.</p>
+<p>Enjoy!</p>
+<?php require_once __DIR__ . '/partials/footer.php'; ?>
