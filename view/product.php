f8112571e421dd836ceb6d92b994d7923c3efb00
David Higley <5118164+MrH100000@users.noreply.github.com>
2022-04-22 13:41:36 -0500

    Fix card styling

--- view/product.php
+++ view/product.php
@@ -1,18 +1,21 @@
 <?php require_once __DIR__ . '/partials/header.php'; ?>
 <center>
-<div>
-<h1>All Products:</h1>
-</div>
+    <h1>All Products:</h1>
+</center>
+<div class="productCards">
     <?php foreach ($productList as $product): ?>
 	    <div class='productCard'>
-            <br>
-            <img src="<?php echo $product['ProductImage']?>" alt="<?php echo $product["Name"]; ?>" style="height:150px">
-            <h2><?php echo $product["Name"]; ?> </h2>
-            <p><?php echo "$". number_format($product["Price"]); ?></p>
-            <p><?php echo $product["Description"]; ?> </p>
-            <p> <button class='productButton'> Add to Cart </button></p>
+            <img src="<?php echo $product['ProductImage']?>" alt="<?php echo $product["Name"]; ?>">
+            <div class="productDescription">
+                <h2><?php echo $product["Name"]; ?> </h2>
+                <p><?php echo "$". number_format($product["Price"]); ?></p>
+                <p><?php echo $product["Description"]; ?> </p>
+            </div>
+            <div class="productFooter">
+                <button class='productButton'> Add to Cart </button>
+            </div>
         </div>
     <?php endforeach;?>
-</center>
+</div>
 
 <?php require_once __DIR__ . '/partials/footer.php';?>
