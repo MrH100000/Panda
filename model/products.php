8085c379459e871928f03ce65a41aa3b9479e6ec
David Higley <5118164+MrH100000@users.noreply.github.com>
2022-04-25 10:53:21 -0500

    Remove legacy code

--- model/products.php
+++ model/products.php
@@ -1,12 +1,3 @@
-<?php 
-    // require_once("database.php");
-    // $pull= filter_input(INPUT_POST, 'pull', FILTER_SANITIZE_SPECIAL_CHARS);
-    // $queryProducts="SELECT * FROM products order by productID";
-    // $statement= $db ->prepare($queryProducts);
-    // $statement -> execute();
-    // $productList=$statement -> fetchAll();
-    // $statement ->closeCursor();
-?>
 <?php
 require_once("database.php");
 require_once("Model.php");
@@ -17,10 +8,6 @@ class Products extends Model {
         if (static::$db === null) {
             static::$db = getDatabase();
         }
-        // $this->lastNameSearchQuery = $db->prepare('SELECT * FROM students WHERE LastName LIKE ?');
-        // $this->favoritePandaSearchQuery = $db->prepare('SELECT * FROM students WHERE FavoritePandaType LIKE ?');
-        // $this->idGreatherThanQuery = $db->prepare('SELECT * FROM students WHERE ID > ?');
-        // $this->newStudentQuery = $db->prepare('INSERT INTO STUDENTS (firstname, lastname, age, favoritepandatype) VALUES (?, ?, ?, ?)');
     }
 
     public function getAll() {
@@ -40,14 +27,6 @@ class Products extends Model {
             handle_error($e->getMessage());
         }
     }
-
-    // public function new($firstName, $lastName, $Age, $favoritePanda) {
-    //     try {
-    //         return $this->newStudentQuery->execute(array($firstName, $lastName, $Age, $favoritePanda));
-    //     } catch(PDOException $e) {
-    //         handle_error($e->getMessage());
-    //     }
-    // }
     
 }
 ?>
