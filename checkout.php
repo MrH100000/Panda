5b0241d7d4e99ca754bb54507fcbb479570dae3c
Danielle <turne292@pnw.edu>
2022-04-27 14:53:19 -0500

    yes

--- checkout.php
+++ checkout.php
@@ -8,7 +8,7 @@
     $total=$orders->getTotal($subtotal);
     if(isset($_POST['submit']))
     {
-        $done=$orders->addToOrder($_POST['street'], $_POST['city'], $_POST['state'], $_POST['zipCode'], $_POST['country'], $_POST['payment'], $total);
+        $done=$orders->addOrder($_POST['street'], $_POST['city'], $_POST['state'], $_POST['zipCode'], $_POST['country'], $_POST['payment'], $total);
         if($done===true)
         {
             $_POST['submit']=false;
