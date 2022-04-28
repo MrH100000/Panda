c6489430883af3b5990bd9c8b2f760d2809e57f7
David Higley <5118164+MrH100000@users.noreply.github.com>
2022-04-28 16:15:38 -0500

    Comment some info on program functionality

--- model/database.php
+++ model/database.php
@@ -1,9 +1,13 @@
 <?php
     require_once(__DIR__ . "/../util/error_handling.php");
+
+    // Return an instance of the database
     function getDatabase() {
         try {
+            // Try to access the database using PDO
             $db = new PDO("sqlite:" . __DIR__ . "/../panda.db");
         } catch(PDOException $e) {
+            // If we can't access the database, display the error page
             handle_error($e->getMessage());
             exit();
         }
--- util/calc_order_total.php
+++ util/calc_order_total.php
@@ -4,18 +4,22 @@ class OrderCalculator {
     protected $SHIPPING_RATE = 0.15;
     protected $TAX_RATE = 0.1;
 
+    // This class calculates different values based on the subtotal provided in the constructor here
     public function __construct($subtotal) {
         $this->subtotal = $subtotal;
     }
 
+    // Return the shipping cost for a given subtotal (Shipping rate * Subtotal)
     public function calcOrderShipping() {
         return $this->SHIPPING_RATE * $this->subtotal;
     }
 
+    // Return the tax for a given subtotal (Tax rate * Subtotal)
     public function calcOrderTax() {
         return $this->TAX_RATE * $this->subtotal;
     }
 
+    // Return the order total (sum of the subtotal plus the shipping fee, plus the tax)
     public function calcOrderTotal() {
         return ($this->calcOrderTax() + $this->subtotal) + $this->calcOrderShipping();
     }
